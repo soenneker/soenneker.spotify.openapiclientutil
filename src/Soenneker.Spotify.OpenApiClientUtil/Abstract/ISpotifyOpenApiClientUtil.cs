2aa98a8a4e9b8f451bb7a4194be0c016c11def3f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:48 -0500

    xml commenting

--- src/Soenneker.Spotify.OpenApiClientUtil/Abstract/ISpotifyOpenApiClientUtil.cs
+++ src/Soenneker.Spotify.OpenApiClientUtil/Abstract/ISpotifyOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Spotify.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ISpotifyOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<SpotifyOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
